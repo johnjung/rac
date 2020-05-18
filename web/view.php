37b6cd374b01f70c63a1efc8d91779560c0deea2
John Jung <john@fieldrotation.com>
2020-05-18 12:19:32 -0500

    Fixes for UV.

--- web/view.php
+++ web/view.php
@@ -1,64 +1,43 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" href="uv/uv.css"/>
+        <script src="https://unpkg.com/resize-observer-polyfill@1.5.1/dist/ResizeObserver.js"></script>
+        <script type="text/javascript" src="uv/uv-assets/js/bundle.js"></script>
+        <script type="text/javascript" src="uv/uv-helpers.js"></script>
+        <script type="text/javascript" src="uv/uv-dist-umd/UV.js"></script>
+        <title>Rose and Chess</title>
+        <style>
+            html, body {
+                 height: 100%;
+                 margin: 0;
+            }   
+            #uv {
+                width: 100%;
+                height: 100%;
+            }   
+        </style>
+    </head>
+    <body>
 
-/*
- * FUNCTIONS
- */
+        <div id="uv"></div>
 
-function print_iiif_html() {
-    global $clean;
-    $cv = (int)$clean['obj'] - 1;
-    printf("https://iiif-viewer.lib.uchicago.edu/uv/./uv.html#?cv=%s&manifest=https://iiif-manifest.lib.uchicago.edu/rac/%s/%s.json", $cv, $clean['doc'], $clean['doc']);
-}
+        <script type="text/javascript">
+            // rose
+            if (window.location.href.indexOf("doc=1380") !== -1) {
+                var manifest_uri = "https://iiif-manifest.lib.uchicago.edu/rac/1380/rac-1380.json"; 
+            } else {
+                var manifest_uri = "https://iiif-manifest.lib.uchicago.edu/rac/0392/rac-0392.json";
+            }
+            var uv = createUV("uv", {
+                manifestUri: manifest_uri,
+                assetsDir: "uv/uv-assets",
+                configUri: "uv-config.json"
+            }, new UV.URLDataProvider());
+        </script>
 
-/*
- * GET URL PARAMS
- */
-
-$clean = array();
-
-$clean['doc'] = '';
-if (isset($_GET['doc'])) {
-	if (ctype_digit($_GET['doc']) && strlen($_GET['doc']) <= 4)
-		$clean['doc'] = sprintf("%04d", $_GET['doc']);
-}
-
-$clean['obj'] = '';
-if (isset($_GET['obj'])) {
-	if (ctype_digit($_GET['obj']) && strlen($_GET['obj']) <= 3)
-		$clean['obj'] = sprintf("%03d", $_GET['obj']);
-}
-?>
-<!DOCTYPE html
-PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-<base href="http://goodspeed.lib.uchicago.edu/view/"/>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv="Pragma" content="no-cache" />
-<title>Rose and Chess</title>
-<style>
-  body {
-    bottom: 0;
-    left: 0;
-    margin: 0;
-    outline: 1px solid red;
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
-  html {
-    background-color: black;
-  }
-</style>
-</head>
-<body>
-
-<!-- IFRAME FOR ZOOMIFY VIEWER -->
-<iframe
-src="<?php print_iiif_html(); ?>" width="100%" height="100%"
-allowfullscreen frameborder="0" marginheight="0" marginwidth="0"
-scrolling="auto" data-sequenceindex="2"></iframe>
-
-</body>
+    </body>
 </html>
